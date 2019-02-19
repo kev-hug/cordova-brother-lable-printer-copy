b0840195a43f92180bd348a76ceb6227bd7ed980
freetail <kevin.hughes@freetail.net>
2019-02-19 14:06:02 +0000

    Initial commit

---
+++ www/printer.js
@@ -0,0 +1,36 @@
+var BrotherPrinter = function () {}
+BrotherPrinter.prototype = {
+    findNetworkPrinters: function (onSuccess, onError) {
+        cordova.exec(onSuccess, onError, 'BrotherPrinter', 'findNetworkPrinters', [])
+    },
+
+    findBluetoothPrinters: function (onSuccess, onError) {
+        cordova.exec(onSuccess, onError, 'BrotherPrinter', 'findBluetoothPrinters', []);
+    },
+
+    findPrinters: function (onSuccess, onError) {
+        cordova.exec(onSuccess, onError, 'BrotherPrinter', 'findPrinters', []);
+    },
+
+    setPrinter: function (printer, onSuccess, onError) {
+        cordova.exec(onSuccess, onError, 'BrotherPrinter', 'setPrinter', [printer]);
+    },
+
+    printViaSDK: function (data, callback) {
+        if (!data || !data.length) {
+            console.log('No data passed in. Expects a bitmap.')
+            return
+        }
+        cordova.exec(callback, function(err){console.log('error: '+err)}, 'BrotherPrinter', 'printViaSDK', [data])
+    },
+
+    sendUSBConfig: function (data, callback) {
+        if (!data || !data.length) {
+            console.log('No data passed in. Expects print payload string.')
+            return
+        }
+        cordova.exec(callback, function(err){console.log('error: '+err)}, 'BrotherPrinter', 'sendUSBConfig', [data])
+    }
+}
+var plugin = new BrotherPrinter()
+module.exports = plugin
